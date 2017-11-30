7fbeea37d7b88ba65df3ac1c6209a579f0207506
clacaille <cesar.lacaille@hotmail.fr>
2017-11-30 14:59:19 +0800

    Update email & phone regex

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,5 @@
 class Request < ApplicationRecord
-  validates :email, uniqueness: true, format: { with: /\A.*@.*\.com\z/ }
-  validates :phone_number, uniqueness: true
+  validates :email, uniqueness: true, format: { with: /(\w+)\@(\w+)\.[a-zA-Z]/ }
+  validates :phone_number, uniqueness: true, format: { with: /((\+)33|0)[1-9](\d{2}){4}/ }
   validates :biography, uniqueness: true, length: { minimum: 300 }
 end
