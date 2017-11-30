2902be0a372116167d92db0f86ce007ec06c2e61
clacaille <cesar.lacaille@hotmail.fr>
2017-11-30 17:39:32 +0800

    Update confirm method to handle both confirm and reconfirm action

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -16,6 +16,22 @@ class RequestsController < ApplicationController
     end
   end
 
+  def confirm
+    @request = Request.find_by_confirm_token(params[:token])
+    if @request
+      @request.confirmed = true
+      @request.expired = false
+      @request.set_position if @request.position.nil?
+      @request.save
+      ReconfirmWorker.perform_in(3.months, @request.id)
+      # flash message to be added
+      redirect_to root_path
+    else
+      # flash message to be added
+      redirect_to root_path
+    end
+  end
+
   private
 
   def request_params
